c3213552bd9c21611bc3011027721d196b939a8e
Tiago09022007 <tlds0902@gmail.com>
2026-02-06 13:24:04 +0000

    FichaTrabalho06

---
+++ FichasPraticas/src/Trabalhoavaliação2/Jogodogalo.java
@@ -0,0 +1,30 @@
+package Trabalhoavaliação2;
+
+import java.util.Scanner;
+
+public class Jogodogalo {
+
+    public  static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int opcao;
+
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
